135c58658e25e58b6291fb44ab2ce534fc1d99c9
divyanshrana <divyansh.rana12@gmail.com>
2025-08-22 22:18:24 +0530

    sandbox issue

--- resume-to-pdf.js
+++ resume-to-pdf.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   // point this to your resume.html file
